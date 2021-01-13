80a66efb9441213ceb62bbceccb7cb9b58a63b47
David Ninh <Dninh12@gmail.com>
2021-01-13 00:22:46 -0600

    Commit 1/13 Sec 13

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router(); // to use express router
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 //Before every route you want to put 3 things(
 //@route    GET api/auth
 //@desc     Test route
 //@access   Public                           )
-router.get('/', (req, res) => res.send('Auth route')); //to create a route use this(this is a test route). fpr call back use arrow funct
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }   catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
