c4163e429c6eb614429f3a4b6f355890a27ad302
David Ninh <Dninh12@gmail.com>
2021-03-31 20:26:27 -0500

    SEC 11,Sec 67/68 FIN 3/31

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -8,19 +8,20 @@ import { addLike, removeLike, deletePost } from '../../actions/post';
 const PostItem = ({
   addLike,
   removeLike,
+  deletePost,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
   showActions,
 }) => (
-  <div class='post bg-white p-1 my-1'>
+  <div className='post bg-white p-1 my-1'>
     <div>
       <Link to={`/profile/${user}`}>
-        <img class='round-img' src={avatar} alt='' />
+        <img className='round-img' src={avatar} alt='' />
         <h4>{name}</h4>
       </Link>
     </div>
     <div>
-      <p class='my-1'>{text}</p>
+      <p className='my-1'>{text}</p>
       <p className='post-date'>Posted on {formatDate(date)}</p>
 
       {showActions && (
@@ -28,19 +29,19 @@ const PostItem = ({
           <button
             onClick={() => addLike(_id)}
             type='button'
-            class='btn btn-light'
+            className='btn btn-light'
           >
-            <i class='fas fa-thumbs-up' />{' '}
+            <i className='fas fa-thumbs-up' />{' '}
             <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
           </button>
           <button
             onClick={() => removeLike(_id)}
             type='button'
-            class='btn btn-light'
+            className='btn btn-light'
           >
-            <i class='fas fa-thumbs-down'></i>
+            <i className='fas fa-thumbs-down'></i>
           </button>
-          <Link to={`/posts/${_id}`} class='btn btn-primary'>
+          <Link to={`/posts/${_id}`} className='btn btn-primary'>
             Discussion{' '}
             {comments.length > 0 && (
               <span className='comment-count'>{comments.length}</span>
@@ -50,9 +51,9 @@ const PostItem = ({
             <button
               onClick={() => deletePost(_id)}
               type='button'
-              class='btn btn-danger'
+              className='btn btn-danger'
             >
-              <i class='fas fa-times'></i>
+              <i className='fas fa-times'></i>
             </button>
           )}
         </Fragment>
