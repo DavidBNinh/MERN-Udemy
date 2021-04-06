343b42c82288a5c091eae56cc256ae543d05872e
David Ninh <Dninh12@gmail.com>
2021-04-06 01:45:36 -0500

    asdf

--- Config/db.js
+++ Config/db.js
@@ -1,24 +1,22 @@
-const mongoose = require('mongoose'); // using mong to conn
-const config = require('config'); // to be able to grab string from default.json
+const mongoose = require('mongoose');
+const config = require('config');
 const db = config.get('mongoURI');
 
-// to connect to mongodb, pass in value. Gives us back a promise
-const connectDB = async () => { //need something to call in server.js
-    try{
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
 
-        });
-
-        console.log('MongoDB Connected...');
-    } catch(err) {
-        console.error(err.message);
-        //Exit process with failure
-        process.exit(1);
-    }
-}
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
